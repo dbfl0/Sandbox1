c39b7993381d52d1fde6efa7ceb15fd54d039637
dbfl0 <85721305+dbfl0@users.noreply.github.com>
2022-03-22 10:56:28 +1100

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
+    res.write("Hello Dale!"); //write a response to the client
     res.end(); //end the response
-  })
+  })w 
   .listen(8080); //the server object listens on port 8080
+wwrani
