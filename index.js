68e7f99d67c332b54d4df677b84362f73be5f6c0
dbfl0 <85721305+dbfl0@users.noreply.github.com>
2022-03-29 12:11:18 +1100

    We now blame Description

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function(req, res) {
-    res.write("Hello Dale!"); //write a response to the client
+    res.write("Hello Blame!"); //write a response to the client
     res.end(); //end the response
   })w 
   .listen(8080); //the server object listens on port 8080
